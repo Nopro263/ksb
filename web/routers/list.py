e4b32348f1656239f416626331dbd3fbb4a97cd5
Noah <82201000+Nopro263@users.noreply.github.com>
2025-01-07 09:57:45 +0100

    added createList

--- web/routers/list.py
+++ web/routers/list.py
@@ -1,11 +1,20 @@
 from fastapi import APIRouter, Body
 from typing import Annotated
 
+from ..database import DB
+from ..schema.list import List
+from ..security import Auth, Clearance
+
 router = APIRouter(prefix="/list")
 
 @router.put("/")
-async def createList():
-    pass
+async def createList(db: DB, auth: Auth[Clearance.OTHER]) -> List:
+    l = List(owner_id=auth.userId)
+    db.add(l)
+    db.commit()
+    db.refresh(l)
+
+    return l
 
 @router.post("/{listId}")
 async def createArticle():

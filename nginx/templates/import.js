20d92b3c880acdfcd17a2e81808e4651aa53796b
Noah <82201000+Nopro263@users.noreply.github.com>
2025-01-17 21:45:25 +0100

    added import html

---
+++ nginx/templates/import.js
@@ -0,0 +1,23 @@
+import Api from "./api.js";
+
+const barcode = document.getElementById("barcode");
+const msg = document.getElementById("msg");
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    try {
+        const article = await Api.import_article(barcode.value);
+        msg.innerHTML = "";
+        if(article.has_already_been_imported) {
+            msg.innerHTML = `Artikel #${article.id} '${article.name}' von #${article.list_id} bereits importiert`;
+            msg.classList.add("error");
+        } else {
+            msg.innerHTML = `Artikel #${article.id} '${article.name}' von #${article.list_id} importiert`;
+            msg.classList.remove("error");
+        }
+    } catch({response, json}) {
+        msg.innerHTML = "Error: " + json.detail;
+        msg.classList.add("error");
+    }
+})

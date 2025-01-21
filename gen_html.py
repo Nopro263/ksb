6b5d48f8f2430c759a16365d97c236cbfc069164
Noah <82201000+Nopro263@users.noreply.github.com>
2025-01-21 09:27:54 +0100

    added more internationalization

--- gen_html.py
+++ gen_html.py
@@ -19,8 +19,68 @@ def main():
         else:
             os.remove(os.path.join(html, h))
     
+    translate()
+    
     for lang in os.listdir(langs):
-        _main(translations={"home": lang}, html=os.path.join(html, lang))
+        _main(translations=parse_translations(os.path.join(langs, lang)), html=os.path.join(html, lang))
+
+def parse_translations(path: str) -> dict:
+    translations = {}
+    with open(path, "r") as f:
+        for line in f.readlines():
+            s = line.split(":    ")
+            translations[s[0]] = s[1].strip()
+    
+    return translations
+
+def write_translations(path: str, translations: dict):
+    if isinstance(translations, list):
+        translations = {key:"" for key in translations}
+
+    with open(path, "w") as f:
+        for key, value in translations.items():
+            f.write(f"{key}:    {value}\n")
+
+def translate():
+    translations = []
+    for file in os.listdir(templates):
+        o, ext = os.path.splitext(file)
+        _, ext2 = os.path.splitext(o)
+        match ext:
+            case ".html":
+                if ext2 == ".jinja2":
+                    continue
+                
+                template = env.get_template(file)
+                output_from_parsed_template = template.render()
+
+                ms = re.findall(r"%(.*?)%", output_from_parsed_template)
+                for m in ms:
+                    if m not in translations:
+                        translations.append(m)
+                continue
+        
+        with open(os.path.join(templates, file), "rb") as f:
+            d = f.read()
+            ms = re.findall(r"%(.*?)%".encode("utf-8"), d)
+            for m in ms:
+                if m not in translations:
+                    translations.append(m.decode("utf-8"))
+            continue
+    
+    for l in os.listdir(langs):
+        lang = os.path.join(langs, l)
+
+        data = parse_translations(lang)
+        for translation in translations:
+            if translation not in data:
+                data[translation] = ""
+        
+        for key in data.keys():
+            if key not in translations:
+                del data[key]
+        
+        write_translations(lang, data)
 
 def _main(translations={}, html=html):
     if os.path.exists(html) and os.path.isdir(html):
@@ -49,12 +109,21 @@ def _main(translations={}, html=html):
         
         with open(os.path.join(html, file), "wb") as fh:
                 with open(os.path.join(templates, file), "rb") as f:
-                    fh.write(f.read())
+                    d = f.read()
+                    for key, value in translations.items():
+                        d = d.replace(f"%{key}%".encode("utf-8"), value.encode("utf-8"))
+                    fh.write(d)
 
 if len(sys.argv) >= 2 and sys.argv[1] == "watch":
-    for changes in watch(templates, raise_interrupt=False):
+    self_changed = False
+    for changes in watch(templates, langs, raise_interrupt=False):
+        if self_changed:
+            self_changed = False
+            continue
         print("Reloading templates")
-        main()
+        if not self_changed:
+            main()
+            self_changed = True
 elif len(sys.argv) >= 2 and sys.argv[1] == "generate":
     translations = []
     for file in os.listdir(templates):
@@ -73,15 +142,36 @@ elif len(sys.argv) >= 2 and sys.argv[1] == "generate":
                     if m not in translations:
                         translations.append(m)
                 continue
+        
+        with open(os.path.join(templates, file), "rb") as f:
+            d = f.read()
+            ms = re.findall(r"%(.*?)%".encode("utf-8"), d)
+            for m in ms:
+                if m not in translations:
+                    translations.append(m.decode("utf-8"))
+            continue
     
-    lang = input("Name of language (eg. de): ")
-    if not os.path.exists(langs):
-        os.mkdir(langs)
-    
-    lang = os.path.join(langs, lang)
+    lang = input("Name of language (eg. de ENTER to append to all): ")
+    if lang:
+        if not os.path.exists(langs):
+            os.mkdir(langs)
+        
+        lang = os.path.join(langs, lang)
 
-    with open(lang, "w") as f:
-        for translation in translations:
-            f.write(f"{translation}:    \n")
+        write_translations(lang, translations)
+    else:
+        for l in os.listdir(langs):
+            lang = os.path.join(langs, l)
+
+            data = parse_translations(lang)
+            for translation in translations:
+                if translation not in data:
+                    data[translation] = ""
+            
+            for key in data.keys():
+                if key not in translations:
+                    del data[key]
+            
+            write_translations(lang, data)
 else:
     main()

628352bdbe503387cd6e21eae57698a55bfd9c5f
Noah <82201000+Nopro263@users.noreply.github.com>
2025-01-27 19:33:31 +0100

    styling

--- nginx/templates/sellers.js
+++ nginx/templates/sellers.js
@@ -9,5 +9,5 @@ const sell = await Api.get_users();
 let i = 1;
 
 sell.forEach(seller => {
-    sellers.innerHTML += `<tr id="S${seller.id}" class="article"><td>${i++}</td><td>${seller.nickname}</td><td>${seller.first_name + " " + seller.last_name}</td><td>${seller.email}</td><td><a href="/seller?id=${seller.id}">Ansehen</a></td></tr>`;
+    sellers.innerHTML += `<tr id="S${seller.id}" class="article"><td>${i++}</td><td>${seller.nickname}</td><td>${seller.first_name + " " + seller.last_name}</td><td>${seller.email}</td><td><a href="/seller?id=${seller.id}" class="success button">Ansehen</a></td></tr>`;
 });

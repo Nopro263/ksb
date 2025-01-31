1ee015b32f048d9d1687b0e8b34ead6d8610ff09
Noah <82201000+Nopro263@users.noreply.github.com>
2025-01-31 22:30:14 +0100

    added display route

--- web/main.py
+++ web/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException
 
 from .routers.users import router as users_router
 from .routers.list import router as list_router
@@ -21,6 +22,25 @@ app.include_router(invoice_router)
 
 @app.get("/stats")
 def stats(db: DB, auth: Auth[Clearance.EMPLOYEE]) -> StatsResponse:
+    amount_articles = db.exec(select(func.count()).where(Article.deleted == False)).one() or 0
+    imported = db.exec(select(func.count()).where(Article.imported == True)).one() or 0
+    sold = db.exec(select(func.count()).where(Article.invoice_id != None)).one() or 0
+    sold_value = db.exec(select(func.sum(Article.price)).where(Article.invoice_id != None)).one() or 0
+    total_value = db.exec(select(func.sum(Article.price)).where(Article.deleted == False)).one() or 0
+
+    return StatsResponse(
+        imported=imported,
+        amount_articles=amount_articles,
+        sold=sold,
+        sold_value=sold_value,
+        total_value=total_value
+    )
+
+@app.get("/display")
+def stats(db: DB, token: str) -> StatsResponse:
+    if(token != os.environ["ACCESS_TOKEN"]):
+        raise HTTPException(status_code=403, detail="not allowed")
+
     amount_articles = db.exec(select(func.count()).where(Article.deleted == False)).one() or 0
     imported = db.exec(select(func.count()).where(Article.imported == True)).one() or 0
     sold = db.exec(select(func.count()).where(Article.invoice_id != None)).one() or 0
